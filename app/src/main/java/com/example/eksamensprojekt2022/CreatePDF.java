d3e74f61dec16608feb7fbf57a5f9e39b74366d3
JulieVedel <Juli76k0@edu.easj.dk>
2022-05-23 09:37:48 +0200

    Dynamic header

--- app/src/main/java/com/example/eksamensprojekt2022/CreatePDF.java
+++ app/src/main/java/com/example/eksamensprojekt2022/CreatePDF.java
@@ -1,12 +1,13 @@
 package com.example.eksamensprojekt2022;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Environment;
-import android.preference.PreferenceActivity;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,18 +15,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.eksamensprojekt2022.Objeckts.InspectionInformation;
 import com.example.eksamensprojekt2022.Objeckts.ProjectInformation;
 import com.example.eksamensprojekt2022.Objeckts.QuestionGroup;
+import com.itextpdf.io.font.PdfEncodings;
 import com.itextpdf.io.image.ImageData;
 import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.colors.ColorConstants;
 import com.itextpdf.kernel.events.Event;
 import com.itextpdf.kernel.events.IEventHandler;
 import com.itextpdf.kernel.events.PdfDocumentEvent;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.kernel.pdf.canvas.CanvasArtifact;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.xobject.PdfFormXObject;
 import com.itextpdf.layout.Canvas;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.borders.Border;
@@ -34,16 +39,26 @@ import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.element.Text;
+import com.itextpdf.layout.layout.LayoutArea;
+import com.itextpdf.layout.layout.LayoutContext;
+import com.itextpdf.layout.layout.LayoutResult;
 import com.itextpdf.layout.property.HorizontalAlignment;
 import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.layout.property.VerticalAlignment;
+import com.itextpdf.layout.renderer.DocumentRenderer;
+import com.itextpdf.layout.renderer.TableRenderer;
+
+import org.apache.commons.io.IOUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 
 public class CreatePDF extends AppCompatActivity {
@@ -53,50 +68,52 @@ public class CreatePDF extends AppCompatActivity {
     Context context;
     MySQL mysql = new MySQL();
     int orderID;
+    PdfFont freeSansFont;
+    PdfFont dejavuSansFont;
+    protected PdfFormXObject template;
 
 
     public void createPdf(Context context) throws IOException {
         this.context = context;
-        orderID = 2;
+        orderID = 27;
         answers = answersInList();
         ProjectInformation projectInfo = mysql.projectInfo(orderID);
-        InspectionInformation inspectionInfo = mysql.inspectionInfo(orderID);
+        InspectionInformation inspectionInfo = InspectionInformation.getInstance();
+        inspectionInfo.setInspectionDate(new Date(2022, 5, 19));
+        inspectionInfo.setInspectorName("Julie");
+        inspectionInfo.setInspectorInformationID(29);
+        inspectionInfo.setFk_projectID(1);
+        inspectionInfo.setFk_roomID(5);
+        inspectionInfo.setRoomName("Loftet");
+
+
+        AssetManager am = context.getAssets();
+        try (InputStream inStream = am.open("freesans.ttf")) {
+            byte[] fontData = IOUtils.toByteArray(inStream);
+            freeSansFont = PdfFontFactory.createFont(fontData, PdfEncodings.IDENTITY_H);
+        }
 
         Table personalInfo = createTablePersonalInfo(projectInfo, inspectionInfo);
         Table questions = createTableQuestions();
-        Table questionsPage2 = createTableQuestions();
 
         String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
-        File file = new File(pdfPath, projectInfo.getCustomerAddress() + " - " + projectInfo.getCustomerCity() + ".pdf");
+        File file = new File(pdfPath, "sagsnummer_" + projectInfo.getProjectInformationID() + ".pdf");
         OutputStream outputstream = new FileOutputStream(file);
 
         PdfWriter writer = new PdfWriter(file);
         PdfDocument pdfDocument = new PdfDocument(writer);
         Document document = new Document(pdfDocument);
 
-        /*PdfPage page = pdfDocument.addNewPage();
-        PdfCanvas pdfCanvas = new PdfCanvas(page);
+        //handler der sætter header ind når der er sideskift
+        TableHeaderEventHandler handler = new TableHeaderEventHandler(document);
+        pdfDocument.addEventHandler(PdfDocumentEvent.END_PAGE, handler);
+        //Tilføjet margin så headeren starter længere nede på siden
+        float topMargin = 30 + handler.getTableHeight();
+        document.setMargins(topMargin, 36, 36, 36);
 
-        Rectangle rectangle = new Rectangle(0, 0, 100, 100);
 
-        com.itextpdf.layout.Canvas canvas = new Canvas(pdfCanvas, pdfDocument, rectangle);
-        canvas.add(createHeader(1));
-
-        canvas.close();*/
-
-        /*pdfDocument.addEventHandler(PdfDocumentEvent.START_PAGE, new IEventHandler() {
-            @Override
-            public void handleEvent(Event event) {
-                createHeader(1);
-            }
-        });*/
-
-        document.add(createHeader(1));
-        document.add(new Paragraph("Elinstallation - Vertifikation af mindre elinstallation").setFontSize(18f).setPaddingTop(5f));
-
+        document.add(new Paragraph("Elinstallation - Vertifikation af mindre elinstallation").setFontSize(18f));
         document.add(personalInfo);
-
-
 
         ArrayList<String> questionsGenerelt = mysql.getQuestionsFromGroup(1);
         ArrayList<String> questionsTavlen = mysql.getQuestionsFromGroup(2);
@@ -110,22 +127,14 @@ public class CreatePDF extends AppCompatActivity {
 
         createQuestions(questions, questionGroupTitle, questionsGenerelt);
         createQuestions(questions, questionGroupTitle, questionsTavlen);
+        createQuestions(questions, questionGroupTitle, questionsInstallation);
+        createQuestions(questions, questionGroupTitle, questionsIndbygningsarmaturer);
+        createQuestions(questions, questionGroupTitle, questionsBeskyttelsesledere);
+        createQuestions(questions, questionGroupTitle, questionsFejlbeskyttelse);
 
         document.add(questions);
 
         document.add(new AreaBreak());
-        document.add(createHeader(2));
-        answerOptions(questionsPage2);
-
-        createQuestions(questionsPage2, questionGroupTitle, questionsInstallation);
-        createQuestions(questionsPage2, questionGroupTitle, questionsIndbygningsarmaturer);
-        createQuestions(questionsPage2, questionGroupTitle, questionsBeskyttelsesledere);
-        createQuestions(questionsPage2, questionGroupTitle, questionsFejlbeskyttelse);
-
-        document.add(questionsPage2);
-
-        document.add(new AreaBreak());
-        document.add(createHeader(3));
         document.add(new Paragraph("Måleresultater").setFontSize(18f).setPaddingTop(5f));
 
         document.add(createTableKredsdetaljer(5));
@@ -133,10 +142,90 @@ public class CreatePDF extends AppCompatActivity {
         document.add(createTableKortslutning(5));
         document.add(createTableBemaerkninger());
 
+
         document.close();
         Toast.makeText(context, "Pdf Created", Toast.LENGTH_LONG).show();
+        
     }
 
+    private class TableHeaderEventHandler implements IEventHandler {
+        private Table table;
+        private float tableHeight;
+        private Document doc;
+
+        public TableHeaderEventHandler(Document doc) {
+            this.doc = doc;
+            initTable();
+
+            TableRenderer renderer = (TableRenderer) table.createRendererSubTree();
+            renderer.setParent(new DocumentRenderer(doc));
+
+            // Simulate the positioning of the renderer to find out how much space the header table will occupy.
+            LayoutResult result = renderer.layout(new LayoutContext(new LayoutArea(0, PageSize.A4)));
+            tableHeight = result.getOccupiedArea().getBBox().getHeight();
+        }
+
+        @Override
+        public void handleEvent(Event currentEvent) {
+            PdfDocumentEvent docEvent = (PdfDocumentEvent) currentEvent;
+            PdfDocument pdfDoc = docEvent.getDocument();
+            PdfPage page = docEvent.getPage();
+            int pageNum = docEvent.getDocument().getPageNumber(page);
+            PdfCanvas canvas = new PdfCanvas(page.newContentStreamBefore(), page.getResources(), pdfDoc);
+            PageSize pageSize = pdfDoc.getDefaultPageSize();
+            float coordX = pageSize.getX() + doc.getLeftMargin();
+            float coordY = pageSize.getTop() - doc.getTopMargin();
+            float width = pageSize.getWidth() - doc.getRightMargin() - doc.getLeftMargin();
+            float height = getTableHeight();
+            Rectangle rect = new Rectangle(coordX, coordY, width, height);
+
+
+            new Canvas(canvas, pdfDoc, rect)
+                    .add(createHeader(pageNum))
+                    .close();
+        }
+
+        public float getTableHeight() {
+            return tableHeight;
+        }
+
+        private void initTable() {
+
+            table = createHeader(1);;
+            table.useAllAvailableWidth();
+            table.setMarginBottom(15);
+
+        }
+
+        public Table createHeader(int page) {
+            Drawable drawable = context.getDrawable(R.drawable.zealand);
+            Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] bitmapData = stream.toByteArray();
+
+            ImageData imageData = ImageDataFactory.create(bitmapData);
+            Image logo = new Image(imageData);
+            logo.setHorizontalAlignment(HorizontalAlignment.CENTER);
+            logo.setHeight(54f);
+
+
+            float[] columnWidth = {2000f, 2000f, 2000f};
+            Table header = new Table(columnWidth);
+            header.setHorizontalAlignment(HorizontalAlignment.CENTER);
+            header.setTextAlignment(TextAlignment.CENTER);
+            header.setPadding(0);
+
+            header.addCell(new Cell(4,1).add(logo).setHorizontalAlignment(HorizontalAlignment.CENTER).setVerticalAlignment(VerticalAlignment.MIDDLE));
+            header.addCell(new Cell(4,1).add(new Paragraph("Tjekliste").setFontSize(36f).setFontColor(ColorConstants.ORANGE).setHorizontalAlignment(HorizontalAlignment.CENTER).setVerticalAlignment(VerticalAlignment.BOTTOM)));
+            header.addCell(new Cell().add(new Paragraph("").setHeight(14f)));
+            header.addCell(new Cell().add(new Paragraph("").setHeight(14f)));
+            header.addCell(new Cell().add(new Paragraph("Side " + page /*+ " af " + total*/)));
+            header.addCell(new Cell().add(new Paragraph("Elinstallation")));
+
+            return header;
+        }
+    }
 
     public String getQuestionGroupTitle(ArrayList<QuestionGroup> group) {
         String result = group.get(questionGroupIndex).getQuestionGroupID() + ". " + group.get(questionGroupIndex).getTitle() + ":";
@@ -266,10 +355,11 @@ public class CreatePDF extends AppCompatActivity {
         personalInfo.setPadding(0);
 
         personalInfo.addCell(new Cell(1, 3).add(new Paragraph("Kundenavn: " + projectInfo.getCustomerName())));
-        personalInfo.addCell(new Cell(1, 3).add(new Paragraph("Adresse: " + projectInfo.getCustomerAddress())));
+        personalInfo.addCell(new Cell(1, 2).add(new Paragraph("Adresse: " + projectInfo.getCustomerAddress())));
+        personalInfo.addCell(new Cell(1, 1).add(new Paragraph("Område: " + inspectionInfo.getRoomName())));
         personalInfo.addCell(new Cell().add(new Paragraph("Post nr: " + projectInfo.getCustomerPostalCode())));
         personalInfo.addCell(new Cell().add(new Paragraph("By: " + projectInfo.getCustomerCity())));
-        personalInfo.addCell(new Cell().add(new Paragraph("Ordernummer: " + projectInfo.getProjectInformationID())));
+        personalInfo.addCell(new Cell().add(new Paragraph("Sagsnummer: " + projectInfo.getProjectInformationID())));
         personalInfo.addCell(new Cell(1, 3).add(new Paragraph("Identifikation af installationen: " + projectInfo.getInstallationIdentification())));
         personalInfo.addCell(new Cell(1, 3).add(new Paragraph("Installationen er udført af: " + projectInfo.getInstallationName())));
         personalInfo.addCell(new Cell(1, 2).add(new Paragraph("Verifikation af installationen er udført af: " + inspectionInfo.getInspectorName())));
@@ -305,12 +395,12 @@ public class CreatePDF extends AppCompatActivity {
 
         table.addCell(new Cell(1,8).add(new Paragraph("Kredsdetaljer").setTextAlignment(TextAlignment.LEFT)).setBackgroundColor(ColorConstants.GRAY));
         table.addCell(new Cell().add(new Paragraph("Gruppe")));
-        table.addCell(new Cell().add(new Paragraph("OB (I\u2099)")));
+        table.addCell(new Cell().add(new Paragraph().add(new Text("OB (I")).add(new Text("n").setFontSize(6)).add(new Text(")"))));
         table.addCell(new Cell().add(new Paragraph("Karakteristik")));
         table.addCell(new Cell().add(new Paragraph("Tværsnit")));
         table.addCell(new Cell().add(new Paragraph("Maks. OB")));
-        table.addCell(new Cell().add(new Paragraph("Zs")));
-        table.addCell(new Cell().add(new Paragraph("RA")));
+        table.addCell(new Cell().add(new Paragraph().add(new Text("Z")).add(new Text("S").setFontSize(6))));
+        table.addCell(new Cell().add(new Paragraph().add(new Text("R")).add(new Text("A").setFontSize(6))));
         table.addCell(new Cell().add(new Paragraph("Isolation")));
 
         if (rows > 0) {
@@ -320,14 +410,14 @@ public class CreatePDF extends AppCompatActivity {
                 table.addCell(new Cell().add(new Paragraph("Boom boom").setTextAlignment(TextAlignment.LEFT)));
                 table.addCell(new Cell().add(new Paragraph("54" + " mm\u00B2").setTextAlignment(TextAlignment.RIGHT)));
                 table.addCell(new Cell().add(new Paragraph("6" + " A").setTextAlignment(TextAlignment.RIGHT)));
-                table.addCell(new Cell(1, 2).add(new Paragraph("9" + " Ohm").setTextAlignment(TextAlignment.RIGHT)));
-                table.addCell(new Cell().add(new Paragraph("25" + " Ohm").setTextAlignment(TextAlignment.RIGHT)));
+                table.addCell(new Cell(1, 2).add(new Paragraph("9" + " \u2126").setFont(freeSansFont).setTextAlignment(TextAlignment.RIGHT)));
+                table.addCell(new Cell().add(new Paragraph("25" + " M\u2126").setFont(freeSansFont).setTextAlignment(TextAlignment.RIGHT)));
             }
         }
 
         table.addCell(new Cell(1, 8).setBorder(Border.NO_BORDER).setHeight(10f));
         table.addCell(new Cell(1, 5).add(new Paragraph("Overgangsmodstand for jordingsleder og jordelektrode R:").setTextAlignment(TextAlignment.LEFT)).setBorderRight(Border.NO_BORDER));
-        table.addCell(new Cell(1, 3).add(new Paragraph("8954" + " Ohm").setTextAlignment(TextAlignment.RIGHT)).setBorderLeft(Border.NO_BORDER));
+        table.addCell(new Cell(1, 3).add(new Paragraph("8954" + " \u2126").setFont(freeSansFont).setTextAlignment(TextAlignment.RIGHT)).setBorderLeft(Border.NO_BORDER));
         table.addCell(new Cell(1, 8).setBorder(Border.NO_BORDER).setHeight(10f));
 
 
@@ -340,6 +430,7 @@ public class CreatePDF extends AppCompatActivity {
         table.setTextAlignment(TextAlignment.CENTER);
         table.setFontSize(10f);
         table.setPadding(0);
+        table.setFont(freeSansFont);
 
         table.addCell(new Cell(1,8).add(new Paragraph("Afprøvning af RCD’er").setTextAlignment(TextAlignment.LEFT)).setBackgroundColor(ColorConstants.GRAY));
         table.addCell(new Cell(2, 1).add(new Paragraph("")));
@@ -349,12 +440,12 @@ public class CreatePDF extends AppCompatActivity {
 
         //Trekant virker ikke
         table.addCell(new Cell().add(new Paragraph("RCD")));
-        table.addCell(new Cell().add(new Paragraph("0º 1xI∆n")));
-        table.addCell(new Cell().add(new Paragraph("180º 1xI∆n")));
-        table.addCell(new Cell().add(new Paragraph("0º 5xI∆n")));
-        table.addCell(new Cell().add(new Paragraph("0º ½xI∆n")));
-        table.addCell(new Cell().add(new Paragraph("0º 1xI∆n")));
-        table.addCell(new Cell().add(new Paragraph("180º 1xI∆n")));
+        table.addCell(new Cell().add(new Paragraph().add(new Text("0º 1xI")).add(new Text("\u0394n").setFontSize(6))));
+        table.addCell(new Cell().add(new Paragraph().add(new Text("180º 1xI")).add(new Text("\u0394n").setFontSize(6))));
+        table.addCell(new Cell().add(new Paragraph().add(new Text("0º 5xI")).add(new Text("\u0394n").setFontSize(6))));
+        table.addCell(new Cell().add(new Paragraph().add(new Text("0º ½xI")).add(new Text("\u0394n").setFontSize(6))));
+        table.addCell(new Cell().add(new Paragraph().add(new Text("0º 1xI")).add(new Text("\u0394n").setFontSize(6))));
+        table.addCell(new Cell().add(new Paragraph().add(new Text("180º 1xI")).add(new Text("\u0394n").setFontSize(6))));
         table.addCell(new Cell().add(new Paragraph("OK")));
 
         if (rows > 0) {
@@ -385,10 +476,10 @@ public class CreatePDF extends AppCompatActivity {
         table.addCell(new Cell(1,4).add(new Paragraph("Spændingsfald").setTextAlignment(TextAlignment.LEFT)).setBackgroundColor(ColorConstants.GRAY));
 
         table.addCell(new Cell().add(new Paragraph("Gruppe")));
-        table.addCell(new Cell().add(new Paragraph("Ik")));
+        table.addCell(new Cell().add(new Paragraph().add(new Text("I")).add(new Text("k").setFontSize(6))));
         table.addCell(new Cell(1, 2).add(new Paragraph("Målt i punkt")));
         table.addCell(new Cell().add(new Paragraph("Gruppe")));
-        table.addCell(new Cell().add(new Paragraph("ΔU")));
+        table.addCell(new Cell().add(new Paragraph().add(new Text("Δ").setFontSize(6).setFont(freeSansFont)).add(new Text("U"))));
         table.addCell(new Cell(1, 2).add(new Paragraph("Målt i punkt")));
 
         if (rows > 0) {
@@ -419,8 +510,6 @@ public class CreatePDF extends AppCompatActivity {
 
         return table;
     }
-
-
 
 }
 
