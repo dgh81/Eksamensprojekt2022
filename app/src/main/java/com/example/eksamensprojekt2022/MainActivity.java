cd1c436a92f6813ffb7a556def76a704c134074e
dgh81 <89967372+dgh81@users.noreply.github.com>
2022-05-26 18:51:20 +0200

    Flyttet kamera logik til PicFragment fra MainActivity

--- app/src/main/java/com/example/eksamensprojekt2022/MainActivity.java
+++ app/src/main/java/com/example/eksamensprojekt2022/MainActivity.java
@@ -1,64 +1,34 @@
 package com.example.eksamensprojekt2022;
 
-
-import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.FileProvider;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
-//import com.example.eksamensprojekt2022.Objeckts.EmailIntent;
 import com.example.eksamensprojekt2022.Objeckts.EmailThread;
-import com.example.eksamensprojekt2022.Objeckts.FileHandler;
 import com.example.eksamensprojekt2022.Objeckts.User;
 import com.example.eksamensprojekt2022.ui.Document.SelectDocumentAndRoomActivityActivity;
 import com.example.eksamensprojekt2022.ui.Login.LoginActivity;
 import com.example.eksamensprojekt2022.ui.Login.LoginSaveData;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.SQLException;
-
-//import static com.example.eksamensprojekt2022.CreatePDF.pdfUri;
-import static com.example.eksamensprojekt2022.CreatePDF.pdfFile;
-import static com.example.eksamensprojekt2022.Objeckts.FileHandler.currentImagePath;
-import static com.example.eksamensprojekt2022.ui.Document.PicFragment.picFragmentImageView;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button idBtnGeneratePDF;
 
-    //Camera
-    ImageView imageViewThumbnail;
-    ImageView fragmentImageView;
-    Bitmap bitmapFromTempFile;
-    public static int CAMERA_CODE = 100;
-    Intent cameraIntent;
-
-    Uri imageUri;
-
     public static Uri pdfUri;
-
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -67,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 /*        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);*/
-
-
 
         LoginSaveData.getInstance().context = this;
 
@@ -129,47 +97,6 @@ public class MainActivity extends AppCompatActivity {
         ab.setDisplayShowTitleEnabled(true);
         ab.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.yellow)));
 
-        //TODO CAMERA:
-        imageViewThumbnail = findViewById(R.id.imageViewThumbnail);
-    }
-
-    public void takePictureButtonClick(View view) {
-        try {
-            cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-            File imageFile = null;
-            try{
-                imageFile= new FileHandler().getImageFile();
-            }catch (IOException e){
-                e.printStackTrace();
-            }
-            if(imageFile!=null){
-                imageUri = FileProvider.getUriForFile(MainActivity.this,"com.example.eksamensprojekt2022.fileprovider",imageFile);
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
-                startActivityForResult(cameraIntent, CAMERA_CODE);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void savePictureButtonClick(View view) {
-        String encodedImageString = new FileHandler().bitmapEncodeToBaseString(bitmapFromTempFile);
-        MySQL mysql = new MySQL();
-        mysql.createBitmapString(encodedImageString, 1, "sagnr + rumnavn + username: + comment?");
-    }
-
-
-    //Camera listener:
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_CODE && resultCode == RESULT_OK) {
-            File imgFile = new File(currentImagePath);
-            String path = imgFile.getAbsolutePath();
-            bitmapFromTempFile = BitmapFactory.decodeFile(path);
-            fragmentImageView = picFragmentImageView;
-            fragmentImageView.setImageBitmap(bitmapFromTempFile);
-        }
     }
 
     @Override
@@ -201,148 +128,10 @@ public class MainActivity extends AppCompatActivity {
         User loggedInUser = mysql.logUserIn("Julie", "123");
     }
 
-    public void sendPdfAsEmail(View view) throws InterruptedException, SQLException, IOException {
-        EmailThread thread = new EmailThread(MainActivity.this);
-        thread.start();
+    //TODO Flyt til UserCase
+    public void userSendsPdfAsEmail(View view) {
+        EmailThread emailIntent = new EmailThread(MainActivity.this);
+        emailIntent.start();
     }
-
-/*    Thread emailThread = new Thread("emailThread") {
-
-            public void run(){
-                System.out.println("run by: " + getName());
-                Uri pdfUri = FileProvider.getUriForFile(MainActivity.this,"com.example.eksamensprojekt2022.fileprovider",pdfFile);
-                final Intent emailIntent = new Intent( Intent.ACTION_SEND);
-                //Set permissions
-                List<ResolveInfo> resInfoList = MainActivity.this.getPackageManager().queryIntentActivities(emailIntent, PackageManager.MATCH_DEFAULT_ONLY);
-                for (ResolveInfo resolveInfo : resInfoList) {
-                    String packageName = resolveInfo.activityInfo.packageName;
-                    MainActivity.this.grantUriPermission(packageName, pdfUri,
-                            Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                }
-                //Set permissions
-                emailIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                emailIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-                //Build email contents
-                emailIntent.setType("plain/text");
-                emailIntent.putExtra(Intent.EXTRA_EMAIL,
-                        new String[] { "danielguldberg@gmail.com" });
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT,
-                        "Email Subject");
-                emailIntent.putExtra(Intent.EXTRA_TEXT,
-                        "Email Body");
-                //emailIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
-                emailIntent.putExtra(Intent.EXTRA_STREAM, pdfUri);
-                emailIntent.setDataAndType(pdfUri, getContentResolver().getType(pdfUri));
-                //Start og vælg mail klient
-                startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-
-                //new EmailIntent().run();
-            }
-        };
-        emailThread.start();
-        emailThread.join();*/
-
-
-
-
-      /*  @Override
-        public void run() {
-            System.out.println("run by: " + getName());
-            Uri pdfUri = FileProvider.getUriForFile(MainActivity.this,"com.example.eksamensprojekt2022.fileprovider",pdfFile);
-            final Intent emailIntent = new Intent( Intent.ACTION_SEND);
-            //Set permissions
-            List<ResolveInfo> resInfoList = MainActivity.this.getPackageManager().queryIntentActivities(emailIntent, PackageManager.MATCH_DEFAULT_ONLY);
-            for (ResolveInfo resolveInfo : resInfoList) {
-                String packageName = resolveInfo.activityInfo.packageName;
-                MainActivity.this.grantUriPermission(packageName, pdfUri,
-                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            }
-            //Set permissions
-            emailIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            emailIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-            //Build email contents
-            emailIntent.setType("plain/text");
-            emailIntent.putExtra(Intent.EXTRA_EMAIL,
-                    new String[] { "danielguldberg@gmail.com" });
-            emailIntent.putExtra(Intent.EXTRA_SUBJECT,
-                    "Email Subject");
-            emailIntent.putExtra(Intent.EXTRA_TEXT,
-                    "Email Body");
-            emailIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
-            emailIntent.putExtra(Intent.EXTRA_STREAM, pdfUri);
-            emailIntent.setDataAndType(pdfUri, getContentResolver().getType(pdfUri));
-            //Start og vælg mail klient
-            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-        }
-    };*/
-
-        //sendEmail();
-
-
-        //new FileHandler().getPDFFile()
-
-
-
-        /*final Intent emailIntent = new Intent( android.content.Intent.ACTION_SEND);
-
-        emailIntent.setType("plain/text");
-
-        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL,
-                new String[] { "abc@gmail.com" });
-
-        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
-                "Email Subject");
-
-        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT,
-                "Email Body");
-
-        //emailIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
-        emailIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
-
-        startActivity(Intent.createChooser(
-                emailIntent, "Send mail..."));*/
-        /*
-        Thread t = new Thread(mysql);
-        // t.run(): kør den ene tråd først
-        // t.start(): kør tråde synkront
-        t.start();
-        // join venter på threads er færdige inden resten af koden køres
-        t.join();*/
-
-        //Skal køres hver gang en klasse kalder på runnable MySQL:
-/*        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
-        System.out.println("Klik");
-
-        MySQL mysql = new MySQL();
-        Thread mysqlConnection = new Thread(mysql);
-        mysqlConnection.run();
-        mysqlConnection.join();
-        User loggedInUser = mysql.logUserIn("David", "123");
-
-        if (loggedInUser == null) {
-            MySQL.connection.close();
-            MySQL.connection = null;
-            System.out.println("set connection thread isAlive?: " + mysqlConnection.isAlive());
-            System.out.println("loggedInUser: null");
-            System.out.println("getConnection: " + mysql.connection);
-
-        } else {
-            System.out.println("set connection thread isAlive?: " + mysqlConnection.isAlive());
-            System.out.println("loggedInUser: " + loggedInUser);
-            System.out.println("getConnection: " + mysql.connection);
-            ArrayList<String> questionGroups = mysql.getQuestionGroups();
-
-            //Print alle questiongroups og deres tilhørende questions
-            for (int i = 0; i < questionGroups.size(); i++) {
-                System.out.println(questionGroups.get(i) + ":");
-                ArrayList<Question> questions = mysql.getQuestionsFromGroupTitle(questionGroups.get(i));
-                for (int j = 0; j < questions.size(); j++) {
-                    System.out.println(questions.get(j).getQuestion());
-                }
-            }
-            mysql.getAnswerInfo();
-        }*/
 }
 
--- app/src/main/java/com/example/eksamensprojekt2022/ui/Document/PicFragment.java
+++ app/src/main/java/com/example/eksamensprojekt2022/ui/Document/PicFragment.java
@@ -1,11 +1,20 @@
 package com.example.eksamensprojekt2022.ui.Document;
 
+import static android.app.Activity.RESULT_OK;
+import static com.example.eksamensprojekt2022.Objeckts.FileHandler.currentImagePath;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,13 +22,23 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.example.eksamensprojekt2022.MySQL;
+import com.example.eksamensprojekt2022.Objeckts.FileHandler;
 import com.example.eksamensprojekt2022.R;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class PicFragment extends Fragment {
 
     public static ImageView picFragmentImageView;
     Button takePictureButton;
+    Button savePictureButton;
+    public static Bitmap bitmapFromTempFile;
+
+    public static int CAMERA_CODE = 100;
+    Uri imageUri;
 
     public PicFragment() {
         // Required empty public constructor
@@ -28,26 +47,63 @@ public class PicFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        /*view = inflater.inflate(R.layout.fragment_pic, container, false);
-        takePictureButton = (Button) view.findViewById(R.id.takePictureButton);*/
-        //System.out.println("pictureButton: " + takePictureButton);
-
+        View view = inflater.inflate(R.layout.fragment_pic, container, false);
         return inflater.inflate(R.layout.fragment_pic, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //View view = inflater.inflate(R.layout.fragment_pic, container, false);
-        picFragmentImageView = (ImageView) getView().findViewById(R.id.imageView3);
-    }
+        takePictureButton = (Button) view.findViewById(R.id.takePictureButton);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("test on click button from fragment");
+                try {
+                    android.content.Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    File imageFile = null;
+                    try{
+                        imageFile= new FileHandler().getImageFile();
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                    if(imageFile!=null){
+                        imageUri = FileProvider.getUriForFile(view.getContext(),"com.example.eksamensprojekt2022.fileprovider",imageFile);
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                        startActivityForResult(cameraIntent, CAMERA_CODE);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        savePictureButton = view.findViewById(R.id.savePictureButton);
+        savePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String encodedImageString = new FileHandler().bitmapEncodeToBaseString(bitmapFromTempFile);
+                MySQL mysql = new MySQL();
+                mysql.createBitmapString(encodedImageString, 1, "sagnr + rumnavn + username: + comment?");
+            }
+        });
 
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println(requestCode + " - " + resultCode);
+        if (requestCode == CAMERA_CODE && resultCode == RESULT_OK) {
+            File imgFile = new File(currentImagePath);
+            String path = imgFile.getAbsolutePath();
+            bitmapFromTempFile = BitmapFactory.decodeFile(path);
+            ImageView fragmentImageView = (ImageView) getView().findViewById(R.id.imageView3);
+            fragmentImageView.setImageBitmap(bitmapFromTempFile);
+        }
+    }
 
 }
