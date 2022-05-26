8ebbd03e47819d916635e1c02588a995b9e1b217
dgh81 <89967372+dgh81@users.noreply.github.com>
2022-05-26 19:01:22 +0200

    Kamera >lollipop permission runtime fix

--- app/src/main/java/com/example/eksamensprojekt2022/ui/Document/PicFragment.java
+++ app/src/main/java/com/example/eksamensprojekt2022/ui/Document/PicFragment.java
@@ -3,14 +3,19 @@ package com.example.eksamensprojekt2022.ui.Document;
 import static android.app.Activity.RESULT_OK;
 import static com.example.eksamensprojekt2022.Objeckts.FileHandler.currentImagePath;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
@@ -21,6 +26,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.eksamensprojekt2022.MySQL;
 import com.example.eksamensprojekt2022.Objeckts.FileHandler;
@@ -37,7 +43,8 @@ public class PicFragment extends Fragment {
     Button savePictureButton;
     public static Bitmap bitmapFromTempFile;
 
-    public static int CAMERA_CODE = 100;
+    final int CAMERA_CODE = 100;
+
     Uri imageUri;
 
     public PicFragment() {
@@ -73,9 +80,16 @@ public class PicFragment extends Fragment {
                         e.printStackTrace();
                     }
                     if(imageFile!=null){
-                        imageUri = FileProvider.getUriForFile(view.getContext(),"com.example.eksamensprojekt2022.fileprovider",imageFile);
-                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
-                        startActivityForResult(cameraIntent, CAMERA_CODE);
+                        if (Build.VERSION.SDK_INT >= 21) {
+                            int permissionCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA);
+                            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_CODE);
+                            } else {
+                                imageUri = FileProvider.getUriForFile(view.getContext(),"com.example.eksamensprojekt2022.fileprovider",imageFile);
+                                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                                startActivityForResult(cameraIntent, CAMERA_CODE);
+                            }
+                        }
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -103,6 +117,25 @@ public class PicFragment extends Fragment {
             bitmapFromTempFile = BitmapFactory.decodeFile(path);
             ImageView fragmentImageView = (ImageView) getView().findViewById(R.id.imageView3);
             fragmentImageView.setImageBitmap(bitmapFromTempFile);
+        }
+    }
+
+
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_CODE:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+
+                } else {
+                    Toast.makeText(getContext(), "This Permission is needed for the app to work perfectly!", Toast.LENGTH_SHORT).show();
+                }
+            default:
+                Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+                break;
         }
     }
 
